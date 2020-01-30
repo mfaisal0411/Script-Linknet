4043c5fd2984f5ea96ae917af4d4357a134d454a
mfaisal0411 <57943964+mfaisal0411@users.noreply.github.com>
2020-01-30 13:36:20 +0700

    Update scp.sh

--- scp.sh
+++ scp.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-#password=ubuntu
-file=/home/ubuntu/tes-20200127.doc
+
+file=/home/ubuntu/tes-`date +"%Y%m%d"`.doc
 
 echo "ubuntu" | scp $file ubuntu@192.168.60.130:/home/ubuntu/
-#sleep 0
-#echo "ubuntu"
-#sleep 0
-#exit
